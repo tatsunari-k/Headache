b3aa32a9a89da7470f65d2122cdd49d1aa2d356e
tatsu-k <think.about.time.jin@gmail.com>
2019-01-08 10:47:37 +0900

    Initial Commit

---
+++ project_Headache/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project_Headache
+//
+//  Created by kataoka-tatsunari on 2019/01/08.
+//  Copyright © 2019年 tatsunari-k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
